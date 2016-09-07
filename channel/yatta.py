89a342308448310135ef2c3042077ff518f60a20
gyermolenko <gyermolenko@gmail.com>
2016-09-07 19:15:20 +0300

    Small refactoring in yatta file

--- channel/yatta.py
+++ channel/yatta.py
@@ -3,13 +3,12 @@ from main.secrets import API_KEY
 
 
 BASE_URL = 'https://www.googleapis.com/youtube/'
-api_version = 'v3'
-username = 'McBacon1337'
+API_VERSION = 'v3'
 
 # https://developers.google.com/youtube/v3/docs/channels/list#try-it
 def get_playlist_id(username):
     part = 'contentDetails'
-    URL = BASE_URL + api_version + '/channels?' \
+    URL = BASE_URL + API_VERSION + '/channels?' \
         'part=%(part)s' \
         '&forUsername=%(username)s' \
         '&key=%(API_KEY)s'
@@ -26,7 +25,7 @@ def get_playlist_id(username):
 
 def get_channel_statistics(username):
     part = 'statistics'
-    URL = BASE_URL + api_version + '/channels?' \
+    URL = BASE_URL + API_VERSION + '/channels?' \
         'part=%(part)s' \
         '&forUsername=%(username)s' \
         '&key=%(API_KEY)s'
@@ -50,7 +49,7 @@ def get_channel_statistics(username):
 def get_videos_meta_info(playlistId):
     part = 'snippet'
     fields = 'items/snippet'
-    URL = BASE_URL + api_version + '/playlistItems?' \
+    URL = BASE_URL + API_VERSION + '/playlistItems?' \
         'part=%(part)s' \
         '&fields=%(fields)s' \
         '&playlistId=%(playlistId)s' \
@@ -79,7 +78,7 @@ def get_videos_meta_info(playlistId):
 # https://developers.google.com/youtube/v3/docs/videos/list#try-it
 def get_video_views_and_likes(videoId):
     part = 'statistics'
-    URL = BASE_URL + api_version + '/videos?' \
+    URL = BASE_URL + API_VERSION + '/videos?' \
         'part=%(part)s' \
         '&id=%(videoId)s' \
         '&key=%(API_KEY)s'
@@ -96,10 +95,11 @@ def get_video_views_and_likes(videoId):
 
 
 if __name__ == "__main__":
-    playlistId = get_playlist_id(username)
+    test_username = 'McBacon1337'
+    playlistId = get_playlist_id(test_username)
     # playlistId = 'UUqJ-Xo29CKyLTjn6z2XwYAw'
     # his_last_vid_id = 'EFvbN3K6EA8'
-    channel_stats = get_channel_statistics(username)
+    channel_stats = get_channel_statistics(test_username)
 
     # published_at '2016-03-09T13:21:32.000Z'
     # videos = get_videos_meta_info(playlistId)
